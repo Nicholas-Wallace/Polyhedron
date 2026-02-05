0baa48b5c745adea2935c02e56343a8f261a87d0
Katerina <feliperochasilva123@gmail.com>
2026-02-05 11:07:02 -0300

    Saturation step 1 implemented

--- Polyhedron.jl
+++ Polyhedron.jl
@@ -409,14 +409,109 @@ function is_pinvariant_delay(A, Ad, F; d=0, symetric=true)
 
     lambda = value(lambda)
     K = value.(K)
-    H = value.(H1) + value.(H2)
-    L = value.(L1) + value.(L2)
+    H = value.(H)
+    L = value.(L)
 
     print(termination_status(model))
 
     result = Dict("lambda" => lambda, "K" => K, "H" => H, "L" => L)
 
     return result
+end
+
+# Passo 1 da saturação
+function step1_saturation(A, B, F, X; symetric=true)
+    model = Model() do
+        return NEOSServer.Optimizer(; email = "wallace.lopes.162@ufrn.edu.br", solver = "Knitro")
+    end
+
+    # parâmetros
+
+    n = size(A, 1) # ordem do sistema
+    f = size(F, 1) # numero de linhas de f
+    b = size(B, 1) 
+    b2 = size(B, 2)
+    x = size(X, 1)
+    w = ones(f)
+    q = ones(x)
+
+    lambda = 0.99
+    @variable(model, 0 <= d)
+
+    if symetric
+        @variable(model, G[1:b2, 1:b])
+        @variable(model, K[1:n, 1:n])
+        @variable(model, 0 <= H1[1:f, 1:f])
+        @variable(model, 0 <= H2[1:f, 1:f])
+        @variable(model, 0 <= L1[1:f, 1:f])
+        @variable(model, 0 <= L2[1:f, 1:f])
+        @variable(model, 0 <= M1[1:f, 1:f])
+        @variable(model, 0 <= M2[1:f, 1:f])
+        @variable(model, 0 <= N1[1:f, 1:f])
+        @variable(model, 0 <= N2[1:f, 1:f])
+        @variable(model, 0 <= R1[1:x, 1:f])
+        @variable(model, 0 <= R2[1:x, 1:f])
+
+        @constraint(model, (H1-H2)*F == F*(A + K))
+        @constraint(model, (L1-L2)*F == F*(B*G - K))
+        @constraint(model, (M1-M2)*F == -F*K*(A - I(n)))
+        @constraint(model, (N1-N2)*F == -F*K*B*G)
+        @constraint(model, ((H1 + H2) + (L1 + L2) + d*((M1 + M2) + (N1 + N2)))*w .<= lambda * w)
+        @constraint(model, (R1-R2)*F == X)
+        @constraint(model, (R1+R2)*q <= q)
+
+        @objective(model, Max, d)
+
+        optimize!(model)
+        
+        d = value(d)
+        K = value.(K)
+        H = value.(H1) + value.(H2)
+        L = value.(L1) + value.(L2)
+
+        print(termination_status(model))
+
+        # retorna um dicionário com lambda e as matrizes K, H e L
+        result = Dict("d" => d, "K" => K, "H" => H, "L" => L)
+
+        return result
+    end
+    
+    @variable(model, G[1:b2, 1:b])
+    @variable(model, K[1:n, 1:n])
+    @variable(model, 0 <= H[1:f, 1:f])
+    @variable(model, 0 <= L[1:f, 1:f])
+    @variable(model, 0 <= M[1:f, 1:f])
+    @variable(model, 0 <= N[1:f, 1:f])
+    @variable(model, 0 <= R[1:x, 1:f])
+
+    @constraint(model, H*F == F*(A + K))
+    @constraint(model, L*F == F*(B*G - K))
+    @constraint(model, M*F == -F*K*(A - I(n)))
+    @constraint(model, N*F == -F*K*B*G)
+    @constraint(model, (H + L + d*(M + N))*w .<= lambda * w)
+    @constraint(model, R*F = X)
+    @constraint(model, R*q <= q)
+
+    @objective(model, Max, d)
+
+    optimize!(model)
+
+    d = value(d)
+    K = value.(K)
+    H = value.(H)
+    L = value.(L)
+
+    print(termination_status(model))
+
+    result = Dict("d" => d, "K" => K, "H" => H, "L" => L)
+
+    return result
+end
+
+# FALTA IMPLEMENTAR
+function step2_saturation()
+
 end
 
 # funções auxiliares para gerar a matriz de condições iniciais admissiveis

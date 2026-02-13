a4953ca4b65431a07902d3ce5b37eca7cd7986df
felipe0x <feliperochasilva123@gmail.com>
2026-02-13 17:58:29 -0300

    added all matrices to symetric case and implement pseudo inverse and Y = F*K

--- Polyhedron.jl
+++ Polyhedron.jl
@@ -454,6 +454,13 @@ function step1_saturation(A, B, X, f; lambda=0.99, symetric=true)
     @variable(model, G[1:1, 1:n])
     @variable(model, K[1:n, 1:n])
     @variable(model, F[1:f, 1:n])
+    @variable(model, Y[1:f, 1:n])
+    @variable(model, J[1:n, 1:f])
+
+    @constraint(model, Y == F*K)
+    @constraint(model, J*F == I(n))
+
+    @objective(model, Max, d)
 
     if symetric
         @variable(model, 0 <= H1[1:f, 1:f] <= 100)
@@ -464,31 +471,33 @@ function step1_saturation(A, B, X, f; lambda=0.99, symetric=true)
         @variable(model, 0 <= M2[1:f, 1:f] <= 100)
         @variable(model, 0 <= N1[1:f, 1:f] <= 100)
         @variable(model, 0 <= N2[1:f, 1:f] <= 100)
-        @variable(model, 0 <= R1[1:x, 1:f])
-        @variable(model, 0 <= R2[1:x, 1:f])
+        @variable(model, 0 <= R1[1:x, 1:f] <= 100)
+        @variable(model, 0 <= R2[1:x, 1:f] <= 100)
 
         @constraint(model, (H1-H2)*F == F*(A + K))
         @constraint(model, (L1-L2)*F == F*(B*G - K))
-        @constraint(model, (M1-M2)*F == -F*K*(A - I(n)))
-        @constraint(model, (N1-N2)*F == -F*K*(B*G))
+        @constraint(model, (M1-M2)*F == -Y*(A - I(n)))
+        @constraint(model, (N1-N2)*F == -Y*(B*G))
         @constraint(model, ((H1 + H2) + (L1 + L2) + d*((M1 + M2) + (N1 + N2)))*w .<= lambda * w)
         @constraint(model, (R1-R2)*F == X)
         @constraint(model, (R1+R2)*w .<= q)
 
-        @objective(model, Max, d)
         optimize!(model)
         
         d = value(d)
         F = value.(F)
         K = value.(K)
-        H = value.(H1) + value.(H2)
-        L = value.(L1) + value.(L2)
+        H = value.(H1) - value.(H2)
+        L = value.(L1) - value.(L2)
+        M = value.(M1) - value.(M2)
+        N = value.(M1) - value.(M2)
+        R = value.(R1) - value.(R2)
         G = value.(G)
 
         print(termination_status(model))
 
-        # retorna um dicionário com lambda e as matrizes K, H e L
-        result = Dict("d" => d, "F" => F, "K" => K, "H" => H, "L" => L, "G" => G)
+        # retorna um dicionário com d e as matrizes
+        result = Dict("d" => d, "F" => F, "G" => G, "K" => K, "H" => H, "L" => L, "M" => M, "N" => N, "R" => R)
 
         return result
     end
@@ -497,17 +506,16 @@ function step1_saturation(A, B, X, f; lambda=0.99, symetric=true)
     @variable(model, 0 <= L[1:f, 1:f] <= 200)
     @variable(model, 0 <= M[1:f, 1:f] <= 200)
     @variable(model, 0 <= N[1:f, 1:f] <= 200)
-    @variable(model, 0 <= R[1:x, 1:f])
+    @variable(model, 0 <= R[1:x, 1:f] <= 200)
 
     @constraint(model, H*F == F*(A + K))
     @constraint(model, L*F == F*(B*G - K))
-    @constraint(model, M*F == -F*(A - I(n)))
-    @constraint(model, N*F == -(F*K)*(B*G))
+    @constraint(model, M*F == -Y*(A - I(n)))
+    @constraint(model, N*F == -Y*(B*G))
     @constraint(model, (H + L + d*(M + N))*w .<= lambda * w)
     @constraint(model, R*F == X)
     @constraint(model, R*w .<= q)
 
-    @objective(model, Max, d)
     optimize!(model)
 
     d = value(d)
@@ -527,7 +535,8 @@ function step1_saturation(A, B, X, f; lambda=0.99, symetric=true)
     return result
 end
 
-# FALTA IMPLEMENTAR MUDANÇAS FEITAS NO PASSO 1
+# FALTA IMPLEMENTAR MUDANÇAS FEITAS NO PASSO 1 E MOVER O IF PRA CIMA DEPOIS DE DESCOBRIR 
+# O QUE TEM DE ERRADO NO 1
 function step2_saturation(A, B, X, G, umax, umin, d; f = 4, v=8, lambda=0.99, symetric=true)
     model = Model() do
         return NEOSServer.Optimizer(; email = "wallace.lopes.162@ufrn.edu.br", solver = "Knitro")

93cc3ff2a95c2ab0d01bd6d481180c30d4cf2a3a
felipe0x <feliperochasilva123@gmail.com>
2026-05-27 15:00:01 -0300

    added more exports and changed the saturation.jl include

--- src/Polyhedron.jl
+++ src/Polyhedron.jl
@@ -20,7 +20,7 @@ include("plots/plot_poly.jl")
 
 include("finding_polyhedrons/finding_L_pinvariant.jl")
 include("finding_polyhedrons/finding_L_pinvariant_delay.jl")
-include("finding_polyhedrons/saturation.jl")
+include("finding_polyhedrons/finding_L_pinvarint_segref_delay_sat.jl")
 include("finding_polyhedrons/finding_L_pinvariant_segref.jl")
 include("finding_polyhedrons/finding_L_pinvariant_segref_delay.jl")
 include("finding_polyhedrons/ref_tracking_delay.jl")
@@ -29,6 +29,7 @@ include("trajectories/trajectory_segref.jl")
 
 
 export finding_L_pinvariant, finding_L_pinvariant_delay, finding_L_pinvariant_segref, finding_L_pinvariant_segref_delay,step1_saturation, step2_saturation, is_pinvariant_seg_ref	
+export step1_saturation_segref, step2_saturation_segref
 export is_pinvariant, is_pinvariant_delay
 export plot_poly, get_shape
 export Vertices, get_vertices, get_extreme_vertices, poly_projection, get_extVert_tuple

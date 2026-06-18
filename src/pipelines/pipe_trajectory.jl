a37e3d9a968568e290ca407b30cfd01fa9523bf8
Nicholas Wallace <nicholaswlopes@hotmail.com>
2026-06-18 11:26:44 -0300

    local changes

--- src/pipelines/pipe_trajectory.jl
+++ src/pipelines/pipe_trajectory.jl
@@ -1,18 +1,23 @@
 
-function pipe_trajectory(F::Matrix{T}, A_exp::Matrix{T}, B_exp::Matrix{T}, G::Matrix{T}, d::Int, Ref::Float64; passos=300) where T
+export pipe_trajectory
+
+function pipe_trajectory(F::Matrix, A_exp::Matrix, B_exp::Matrix, E_exp::Matrix, G::Matrix, d::Int, Ref::Float64; passos=300)
     w = ones(size(F, 1))
     nx = size(A_exp, 1)
+
+    T = Polyhedron.poly_projection(F[:,3:4])
+
 
     passos = 300
     r = ones(passos)*Ref # escolher a referência a se seguir 
 
     BG = B_exp * G
 
-    init_cond_F, init_cond_w = Poly.admissable_initCond(A_exp, BG, F, d, w; fixed_d=true)
-    ext_vertices_tuple = Poly.get_extVert_tuple(init_cond_F, init_cond_w, 2, nx)
-    traj = Poly.trajectory_segref_delay(ext_vertices_tuple, A_exp, BG, E_exp, r, passos, d; reverse=false)
+    init_cond_F, init_cond_w = Polyhedron.admissable_initCond(A_exp, BG, F, d, w; fixed_d=true)
+    ext_vertices_tuple = Polyhedron.get_extVert_tuple(init_cond_F, init_cond_w, 2, nx)
+    traj = Polyhedron.trajectory_segref_delay(ext_vertices_tuple, A_exp, BG, E_exp, r, passos, d; reverse=false)
 
-    Poly.plot_poly(T*F[:,1:2], T*ones(size(F, 1)))
+    Polyhedron.plot_poly(T*F[:,1:2], T*ones(size(F, 1)))
 
     x1 = [p[1] for p in traj]
     x2 = [p[2] for p in traj]
